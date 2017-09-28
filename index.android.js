a22d0cc0b4456340967ca24bc3f5206df338f441
Jonathan Kammo <jonathankammo@gmail.com>
2017-09-28 16:28:30 -0500

    Set up boilerplate for audio setup, test google cloud speech api

--- index.android.js
+++ index.android.js
@@ -1,18 +1,92 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable class-methods-use-this */
 
 import React, { Component } from 'react';
+import SoundRecorder from 'react-native-sound-recorder';
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  Button,
   View
 } from 'react-native';
 
+const record = require('node-record-lpcm16');
+
+// Imports the Google Cloud client library
+const Speech = require('@google-cloud/speech');
+const fs = require('fs');
+// Instantiates a client
+const speech = Speech();
+
+// The encoding of the audio file, e.g. 'LINEAR16'
+const encoding = 'LINEAR16';
+
+// The sample rate of the audio file in hertz, e.g. 16000
+const sampleRateHertz = 16000;
+
+// The BCP-47 language code to use, e.g. 'en-US'
+const languageCode = 'en-US';
+
+const request = {
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode
+  },
+  interimResults: false // If you want interim results, set this to true
+};
+
+
+
+// Stream an audio file from disk to the Speech API, e.g. "./resources/audio.raw"
+// fs.createReadStream(filename).pipe(recognizeStream);
+
+// SoundRecorder.start(SoundRecorder.PATH_CACHE + '/test.mp4')
+//     .then(function() {
+//         console.log('started recording');
+//     });
+
+// SoundRecorder.stop()
+//     .then(function(path) {
+//         console.log('stopped recording, audio file saved at: ' + path);
+//     });
+
 export default class wordUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPressed: false,
+    }
+    this.onPress = this.onPress.bind(this);
+  }
+
+  onPress() {
+    console.log(this.state);
+    if (this.state.isPressed) {
+      this.setState({ isPressed: false });
+      SoundRecorder.stop()
+        .then(function (path) {
+          console.log('stopped recording, audio file saved at: ' + path);
+          // Stream the audio to the Google Cloud Speech API
+          const recognizeStream = speech.streamingRecognize(request)
+            .on('error', console.error)
+            .on('data', (data) => {
+              console.log(
+                `Transcription: ${data.results[0].alternatives[0].transcript}`);
+            });
+          fs.createReadStream(path).pipe(recognizeStream);
+        });
+    } else {
+      this.setState({ isPressed: true });
+      SoundRecorder.start(SoundRecorder.PATH_CACHE + '/test.mp4')
+        .then(function () {
+          console.log(SoundRecorder.PATH_CACHE + '/test.mp4');
+        });
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -26,6 +100,12 @@ export default class wordUp extends Component {
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
         </Text>
+        <Button
+          onPress={this.onPress}
+          title="Record"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
       </View>
     );
   }
